134d8833b9b2a9e32f53d3a9ddac9546142314be
Kevin Wang <kevwang76@gmail.com>
2018-04-22 16:16:21 -0400

    make it better

--- database/index.js
+++ database/index.js
@@ -2,7 +2,9 @@ const mysql = require('mysql')
 const mysqlConfig = require('./config.js')
 const Sequelize = require('sequelize')
 
-connection = new Sequelize('workoutworld', 'root', '', mysqlConfig)
+connection = new Sequelize('heroku_ba26fb2fee70382', 'b8f7c4ede32d88', '640671e4', {
+  dialect: 'mysql'
+})
 
 exports.connection = connection
 
